6fc2fecb8b4023465d698cb0472f2752ed6774d7
dsapoetra <angga.dimassaputra@gmail.com>
2017-08-18 15:33:12 +0700

    this is failed to run

--- app/src/main/java/com/example/dimassaputra/zenius/MainActivity.java
+++ app/src/main/java/com/example/dimassaputra/zenius/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.dimassaputra.zenius;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.graphics.Typeface;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView myTextView=(TextView)findViewById(R.id.founder);
+        Typeface typeFace=Typeface.createFromAsset(getAssets(),"fonts/Gotham-Book.otf");
+        myTextView.setTypeface(typeFace);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
